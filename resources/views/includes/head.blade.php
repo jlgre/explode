fc40fd33da0a4a65303e01f7a9ca130a483ec2e2
Luke Green <lukegreen@Lukes-MacBook-Pro.local>
2023-07-02 15:59:46 -0700

    init

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Explode</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    @include('includes.style')
+</head>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('common')
+
+@section('content')
+    <p>Hello world</p>
+@endsection
